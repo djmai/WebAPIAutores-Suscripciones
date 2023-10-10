5657723b5b886a27ddf2a5a37e1885c1daa1fddc
Miguel Martínez <mai.djmai@gmail.com>
2023-10-09 19:27:33 -0600

    Nuevas migraciones

--- Entities/Libro.cs
+++ Entities/Libro.cs
@@ -12,6 +12,7 @@ namespace WebAPIAutores.Entities
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 250, ErrorMessage = "El campo {0} no debe tener más de {1} carácteres")]
         [PrimeraLetraMayuscula]
         public string Titulo { get; set; }
     }
