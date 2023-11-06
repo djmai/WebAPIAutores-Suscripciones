6efb3e9be4e49fc0f99dfa9028e52397cbd17e36
Miguel Martínez <mai.djmai@gmail.com>
2023-11-06 15:08:48 -0600

    Agregando DbSet<LlaveAPI>

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -31,5 +31,6 @@ namespace WebAPIAutores
         public DbSet<Libro> Libros { get; set; }
         public DbSet<Comentario> Comentarios { get; set; }
         public DbSet<AutorLibro> AutoresLibros { get; set; }
+        public DbSet<LlaveAPI> LlavesAPI { get; set; }
     }
 }
