6cb67a6a2e8b0e0d4594d25b0936dc98643855ac
Miguel Martínez <mai.djmai@gmail.com>
2023-11-06 16:10:18 -0600

    Agregando entidad Peticiones

---
+++ Entities/Peticion.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPIAutores.Entities
+{
+	public class Peticion
+	{
+		public int Id { get; set; }
+
+		public int LlaveId { get; set; }
+
+		public DateTime FechaPeticion { get; set; }
+
+		public LlaveAPI Llave { get; set; }
+	}
+}
