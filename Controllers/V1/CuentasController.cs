838b98992924af4299d4e44cc270cc5309d57bc3
Miguel Martínez <mai.djmai@gmail.com>
2023-11-06 14:51:30 -0600

    Agregando el Claim UsuarioId

--- Controllers/V1/CuentasController.cs
+++ Controllers/V1/CuentasController.cs
@@ -16,163 +16,116 @@ using WebAPIAutores.Services;
 
 namespace WebAPIAutores.Controllers.V1
 {
-    [ApiController]
-    [Route("api/v1/cuentas")]
-    public class CuentasController : ControllerBase
-    {
-        private readonly UserManager<IdentityUser> userManager;
-        private readonly IConfiguration configuration;
-        private readonly SignInManager<IdentityUser> signInManager;
-        private readonly HashService hashService;
-        private readonly IDataProtector dataProtector;
+	[ApiController]
+	[Route("api/v1/cuentas")]
+	public class CuentasController : ControllerBase
+	{
+		private readonly UserManager<IdentityUser> userManager;
+		private readonly IConfiguration configuration;
+		private readonly SignInManager<IdentityUser> signInManager;
 
-        public CuentasController(UserManager<IdentityUser> userManager, IConfiguration configuration, SignInManager<IdentityUser> signInManager, IDataProtectionProvider dataProtectionProvider, HashService hashService)
-        {
-            this.userManager = userManager;
-            this.configuration = configuration;
-            this.signInManager = signInManager;
-            this.hashService = hashService;
-            dataProtector = dataProtectionProvider.CreateProtector("ASP.NET Identity");
-        }
+		public CuentasController(UserManager<IdentityUser> userManager, IConfiguration configuration, SignInManager<IdentityUser> signInManager)
+		{
+			this.userManager = userManager;
+			this.configuration = configuration;
+			this.signInManager = signInManager;
+		}
 
-        // Ejemplos de encriptación
-        // [HttpGet("hash/{textoPlano}")]
-        // public ActionResult RealizarHash(string textoPlano)
-        // {
-        //     var resultado1 = hashService.Hash(textoPlano);
-        //     var resultado2 = hashService.Hash(textoPlano);
+		[HttpPost("registrar", Name = "registrarUsuario")] // api/cuentas/registrar
+		public async Task<ActionResult<RespuestaAutenticacion>> Registrar(CredencialesUsuario credencialesUsuario)
+		{
+			var usuario = new IdentityUser { UserName = credencialesUsuario.Email, Email = credencialesUsuario.Email };
+			var resultado = await userManager.CreateAsync(usuario, credencialesUsuario.Password);
 
-        //     return Ok(new
-        //     {
-        //         textoPlano,
-        //         Hash1 = resultado1,
-        //         Hash2 = resultado2,
-        //     });
-        // }
+			if (resultado.Succeeded)
+			{
+				return await ConstruirToken(credencialesUsuario, usuario.Id);
+			}
+			else
+			{
+				return BadRequest(resultado.Errors);
+			}
+		}
 
-        // [HttpGet("encriptar")]
-        // public ActionResult Encriptar()
-        // {
-        //     var textoPlano = "Mi nombre es Pedro";
-        //     var textoCifrado = dataProtector.Protect(textoPlano);
-        //     var textoDesencriptado = dataProtector.Unprotect(textoCifrado);
+		[HttpPost("login", Name = "loginUsuario")]
+		public async Task<ActionResult<RespuestaAutenticacion>> Login(CredencialesUsuario credencialesUsuario)
+		{
+			var resultado = await signInManager.PasswordSignInAsync(credencialesUsuario.Email, credencialesUsuario.Password, isPersistent: false, lockoutOnFailure: false);
 
-        //     return Ok(new
-        //     {
-        //         textoPlano,
-        //         textoCifrado,
-        //         textoDesencriptado
-        //     });
-        // }
+			if (resultado.Succeeded)
+			{
+				var usuario = await userManager.FindByEmailAsync(credencialesUsuario.Email);
+				return await ConstruirToken(credencialesUsuario, usuario.Id);
+			}
+			else
+			{
+				return BadRequest("Login incorrecto");
+			}
+		}
 
-        // [HttpGet("encriptarPorTiempo")]
-        // public ActionResult EncriptarPorTiempo()
-        // {
-        //     var protectorLimitadoPorTiempo = dataProtector.ToTimeLimitedDataProtector();
+		[HttpGet("RenovarToken", Name = "renovarToken")]
+		[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+		public async Task<ActionResult<RespuestaAutenticacion>> Renovar()
+		{
+			var emailClaim = HttpContext.User.Claims.Where(claim => claim.Type == "email").FirstOrDefault();
+			var email = emailClaim.Value;
+			var idClaim = HttpContext.User.Claims.Where(claim => claim.Type == "id").FirstOrDefault();
+			var usuarioId = idClaim.Value;
 
-        //     var textoPlano = "Mi nombre es Pedro";
-        //     var textoCifrado = protectorLimitadoPorTiempo.Protect(textoPlano, lifetime: TimeSpan.FromSeconds(5));
-        //     Thread.Sleep(6000);
-        //     var textoDesencriptado = protectorLimitadoPorTiempo.Unprotect(textoCifrado);
+			var credencialesUsuario = new CredencialesUsuario()
+			{
+				Email = email
+			};
 
-        //     return Ok(new
-        //     {
-        //         textoPlano,
-        //         textoCifrado,
-        //         textoDesencriptado
-        //     });
-        // }
+			return await ConstruirToken(credencialesUsuario, usuarioId);
+		}
 
-        [HttpPost("registrar", Name = "registrarUsuario")] // api/cuentas/registrar
-        public async Task<ActionResult<RespuestaAutenticacion>> Registrar(CredencialesUsuario credencialesUsuario)
-        {
-            var usuario = new IdentityUser { UserName = credencialesUsuario.Email, Email = credencialesUsuario.Email };
-            var resultado = await userManager.CreateAsync(usuario, credencialesUsuario.Password);
+		private async Task<RespuestaAutenticacion> ConstruirToken(CredencialesUsuario credencialesUsuario, string usuarioId)
+		{
+			var claims = new List<Claim>(){
+				new Claim("email", credencialesUsuario.Email),
+				new Claim("id", usuarioId)
+			};
 
-            if (resultado.Succeeded)
-            {
-                return await ConstruirToken(credencialesUsuario);
-            }
-            else
-            {
-                return BadRequest(resultado.Errors);
-            }
-        }
+			var usuario = await userManager.FindByEmailAsync(credencialesUsuario.Email);
+			var claimsDB = await userManager.GetClaimsAsync(usuario);
 
-        [HttpPost("login", Name = "loginUsuario")]
-        public async Task<ActionResult<RespuestaAutenticacion>> Login(CredencialesUsuario credencialesUsuario)
-        {
-            var resultado = await signInManager.PasswordSignInAsync(credencialesUsuario.Email, credencialesUsuario.Password, isPersistent: false, lockoutOnFailure: false);
+			claims.AddRange(claimsDB);
 
-            if (resultado.Succeeded)
-            {
-                return await ConstruirToken(credencialesUsuario);
-            }
-            else
-            {
-                return BadRequest("Login incorrecto");
-            }
-        }
+			var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["llavejwt"]));
+			var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
 
-        [HttpGet("RenovarToken", Name = "renovarToken")]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public async Task<ActionResult<RespuestaAutenticacion>> Renovar()
-        {
-            var emailClaim = HttpContext.User.Claims.Where(claim => claim.Type == "email").FirstOrDefault();
-            var email = emailClaim.Value;
-            var credencialesUsuario = new CredencialesUsuario()
-            {
-                Email = email
-            };
+			var expiracion = DateTime.UtcNow.AddYears(1);
 
-            return await ConstruirToken(credencialesUsuario);
-        }
+			var securityToken = new JwtSecurityToken(
+			  issuer: null,
+			  audience: null,
+			  claims: claims,
+			  expires: expiracion,
+			  signingCredentials: creds
+			);
 
-        private async Task<RespuestaAutenticacion> ConstruirToken(CredencialesUsuario credencialesUsuario)
-        {
-            var claims = new List<Claim>(){
-                new Claim("email", credencialesUsuario.Email)
-            };
+			return new RespuestaAutenticacion()
+			{
+				Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
+				Expiracion = expiracion
+			};
+		}
 
-            var usuario = await userManager.FindByEmailAsync(credencialesUsuario.Email);
-            var claimsDB = await userManager.GetClaimsAsync(usuario);
+		[HttpPost("HacerAdmin", Name = "hacerAdmin")]
+		public async Task<ActionResult> HacerAdmin(EditarAdminDTO editarAdminDTO)
+		{
+			var usuario = await userManager.FindByEmailAsync(editarAdminDTO.Email);
+			await userManager.AddClaimAsync(usuario, new Claim("esAdmin", "1"));
+			return NoContent();
+		}
 
-            claims.AddRange(claimsDB);
-
-            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["llavejwt"]));
-            var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
-
-            var expiracion = DateTime.UtcNow.AddYears(1);
-
-            var securityToken = new JwtSecurityToken(
-              issuer: null,
-              audience: null,
-              claims: claims,
-              expires: expiracion,
-              signingCredentials: creds
-            );
-
-            return new RespuestaAutenticacion()
-            {
-                Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
-                Expiracion = expiracion
-            };
-        }
-
-        [HttpPost("HacerAdmin", Name = "hacerAdmin")]
-        public async Task<ActionResult> HacerAdmin(EditarAdminDTO editarAdminDTO)
-        {
-            var usuario = await userManager.FindByEmailAsync(editarAdminDTO.Email);
-            await userManager.AddClaimAsync(usuario, new Claim("esAdmin", "1"));
-            return NoContent();
-        }
-
-        [HttpPost("RemoverAdmin", Name = "removerAdmin")]
-        public async Task<ActionResult> RemoverAdmin(EditarAdminDTO editarAdminDTO)
-        {
-            var usuario = await userManager.FindByEmailAsync(editarAdminDTO.Email);
-            await userManager.RemoveClaimAsync(usuario, new Claim("esAdmin", "1"));
-            return NoContent();
-        }
-    }
+		[HttpPost("RemoverAdmin", Name = "removerAdmin")]
+		public async Task<ActionResult> RemoverAdmin(EditarAdminDTO editarAdminDTO)
+		{
+			var usuario = await userManager.FindByEmailAsync(editarAdminDTO.Email);
+			await userManager.RemoveClaimAsync(usuario, new Claim("esAdmin", "1"));
+			return NoContent();
+		}
+	}
 }
