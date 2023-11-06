1e528a08305cc61f61640aa60e3e6dd3b33d99a5
Miguel Martínez <mai.djmai@gmail.com>
2023-11-06 16:11:32 -0600

    Agregando DbSet<Peticion>

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -8,29 +8,30 @@ using WebAPIAutores.Entities;
 
 namespace WebAPIAutores
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-        }
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<AutorLibro>().HasKey(al => new { al.AutorId, al.LibroId });
-        }
+			modelBuilder.Entity<AutorLibro>().HasKey(al => new { al.AutorId, al.LibroId });
+		}
 
-        // Comando para iniciar migraciones desde terminal
-        //  dotnet ef migrations add NOMBRE_MIGRACION
+		// Comando para iniciar migraciones desde terminal
+		//  dotnet ef migrations add NOMBRE_MIGRACION
 
-        // Actualizar base de datos
-        // dotnet ef database update
+		// Actualizar base de datos
+		// dotnet ef database update
 
-        public DbSet<Autor> Autores { get; set; }
-        public DbSet<Libro> Libros { get; set; }
-        public DbSet<Comentario> Comentarios { get; set; }
-        public DbSet<AutorLibro> AutoresLibros { get; set; }
-        public DbSet<LlaveAPI> LlavesAPI { get; set; }
-    }
+		public DbSet<Autor> Autores { get; set; }
+		public DbSet<Libro> Libros { get; set; }
+		public DbSet<Comentario> Comentarios { get; set; }
+		public DbSet<AutorLibro> AutoresLibros { get; set; }
+		public DbSet<LlaveAPI> LlavesAPI { get; set; }
+		public DbSet<Peticion> Peticiones { get; set; }
+	}
 }
