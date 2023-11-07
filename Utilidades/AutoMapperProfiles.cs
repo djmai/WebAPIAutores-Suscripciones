c9cd64d43c42e240516e74602456af803e7191b3
Miguel Martínez <mai.djmai@gmail.com>
2023-11-07 13:08:04 -0600

    Agregando mapeo de <RestriccionDominio, RestriccionDominioDTO>

--- Utilidades/AutoMapperProfiles.cs
+++ Utilidades/AutoMapperProfiles.cs
@@ -8,78 +8,79 @@ using WebAPIAutores.Entities;
 
 namespace WebAPIAutores.Utilidades
 {
-    public class AutoMapperProfiles : Profile
-    {
-        public AutoMapperProfiles()
-        {
-            CreateMap<AutorCreacionDTO, Autor>();
-            CreateMap<Autor, AutorDTO>();
-            CreateMap<Autor, AutorDTOConLibros>().ForMember(autorDTO => autorDTO.Libros, opciones => opciones.MapFrom(MapAutorDTOLibros));
+	public class AutoMapperProfiles : Profile
+	{
+		public AutoMapperProfiles()
+		{
+			CreateMap<AutorCreacionDTO, Autor>();
+			CreateMap<Autor, AutorDTO>();
+			CreateMap<Autor, AutorDTOConLibros>().ForMember(autorDTO => autorDTO.Libros, opciones => opciones.MapFrom(MapAutorDTOLibros));
 
-            CreateMap<LibroCreacionDTO, Libro>().ForMember(libro => libro.AutoresLibros, opciones => opciones.MapFrom(MapAutoresLibros));
-            CreateMap<Libro, LibroDTO>().ReverseMap();
-            CreateMap<Libro, LibroDTOConAutores>().ForMember(libroDTO => libroDTO.Autores, opciones => opciones.MapFrom(MapLibroDTOAutores));
-            CreateMap<LibroPatchDTO, Libro>().ReverseMap();
+			CreateMap<LibroCreacionDTO, Libro>().ForMember(libro => libro.AutoresLibros, opciones => opciones.MapFrom(MapAutoresLibros));
+			CreateMap<Libro, LibroDTO>().ReverseMap();
+			CreateMap<Libro, LibroDTOConAutores>().ForMember(libroDTO => libroDTO.Autores, opciones => opciones.MapFrom(MapLibroDTOAutores));
+			CreateMap<LibroPatchDTO, Libro>().ReverseMap();
 
-            CreateMap<ComentarioCreacionDTO, Comentario>();
-            CreateMap<Comentario, ComentarioDTO>();
+			CreateMap<ComentarioCreacionDTO, Comentario>();
+			CreateMap<Comentario, ComentarioDTO>();
 
-            CreateMap<LlaveAPI, LlaveDTO>();
-        }
+			CreateMap<LlaveAPI, LlaveDTO>();
+			CreateMap<RestriccionDominio, RestriccionDominioDTO>();
+		}
 
-        private List<LibroDTO> MapAutorDTOLibros(Autor autor, AutorDTO autorDTO)
-        {
-            var resultado = new List<LibroDTO>();
+		private List<LibroDTO> MapAutorDTOLibros(Autor autor, AutorDTO autorDTO)
+		{
+			var resultado = new List<LibroDTO>();
 
-            if (autor.AutoresLibros == null)
-                return resultado;
+			if (autor.AutoresLibros == null)
+				return resultado;
 
-            foreach (var autorLibro in autor.AutoresLibros)
-            {
-                resultado.Add(new LibroDTO()
-                {
-                    Id = autorLibro.LibroId,
-                    Titulo = autorLibro.Libro.Titulo
-                });
-            }
+			foreach (var autorLibro in autor.AutoresLibros)
+			{
+				resultado.Add(new LibroDTO()
+				{
+					Id = autorLibro.LibroId,
+					Titulo = autorLibro.Libro.Titulo
+				});
+			}
 
-            return resultado;
-        }
+			return resultado;
+		}
 
-        private List<AutorDTO> MapLibroDTOAutores(Libro libro, LibroDTO libroDTO)
-        {
-            var resultado = new List<AutorDTO>();
+		private List<AutorDTO> MapLibroDTOAutores(Libro libro, LibroDTO libroDTO)
+		{
+			var resultado = new List<AutorDTO>();
 
-            if (libro.AutoresLibros == null)
-                return resultado;
+			if (libro.AutoresLibros == null)
+				return resultado;
 
-            foreach (var autorLibro in libro.AutoresLibros)
-            {
-                resultado.Add(new AutorDTO()
-                {
-                    Id = autorLibro.AutorId,
-                    Nombre = autorLibro.Autor.Nombre
-                });
-            }
+			foreach (var autorLibro in libro.AutoresLibros)
+			{
+				resultado.Add(new AutorDTO()
+				{
+					Id = autorLibro.AutorId,
+					Nombre = autorLibro.Autor.Nombre
+				});
+			}
 
-            return resultado;
-        }
+			return resultado;
+		}
 
-        private List<AutorLibro> MapAutoresLibros(LibroCreacionDTO libroCreacionDTO, Libro libro)
-        {
-            var resultado = new List<AutorLibro>();
+		private List<AutorLibro> MapAutoresLibros(LibroCreacionDTO libroCreacionDTO, Libro libro)
+		{
+			var resultado = new List<AutorLibro>();
 
-            if (libroCreacionDTO.AutoresIds == null)
-            {
-                return resultado;
-            }
+			if (libroCreacionDTO.AutoresIds == null)
+			{
+				return resultado;
+			}
 
-            foreach (var autorId in libroCreacionDTO.AutoresIds)
-            {
-                resultado.Add(new AutorLibro() { AutorId = autorId });
-            }
+			foreach (var autorId in libroCreacionDTO.AutoresIds)
+			{
+				resultado.Add(new AutorLibro() { AutorId = autorId });
+			}
 
-            return resultado;
-        }
-    }
+			return resultado;
+		}
+	}
 }
