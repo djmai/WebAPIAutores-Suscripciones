0a7546cc79dca481b2341c9d46c6a7e28b11c0d7
Miguel Martínez <mai.djmai@gmail.com>
2023-11-06 23:48:25 -0600

    Agregando tablas a DbContext

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -33,5 +33,7 @@ namespace WebAPIAutores
 		public DbSet<AutorLibro> AutoresLibros { get; set; }
 		public DbSet<LlaveAPI> LlavesAPI { get; set; }
 		public DbSet<Peticion> Peticiones { get; set; }
+		public DbSet<RestriccionDominio> RestriccionesDominio { get; set; }
+		public DbSet<RestriccionIP> RestricionesIP { get; set; }
 	}
 }
