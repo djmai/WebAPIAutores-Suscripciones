cc0f22fa84fdfbc221adcedcd88e3d86f938dd35
Miguel Martínez <mai.djmai@gmail.com>
2023-11-07 13:38:49 -0600

    Agregando DTO's <RestriccionIP>

---
+++ DTOs/CrearRestriccionesIPDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIAutores.DTOs
+{
+	public class CrearRestriccionesIPDTO
+	{
+		public int LlaveId { get; set; }
+
+		[Required]
+		public string IP { get; set; }
+	}
+}
---
+++ DTOs/RestriccionIPDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPIAutores.DTOs
+{
+	public class RestriccionIPDTO
+	{
+		public int Id { get; set; }
+
+		public int LlaveId { get; set; }
+
+		public string IP { get; set; }
+	}
+}
