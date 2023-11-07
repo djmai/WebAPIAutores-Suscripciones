484ddf4fec2efce53cfbccf15f9a61f24ec96e2d
Miguel Martínez <mai.djmai@gmail.com>
2023-11-07 13:07:47 -0600

    Agregando propiedad List<RestriccionDominioDTO>

--- DTOs/LlaveDTO.cs
+++ DTOs/LlaveDTO.cs
@@ -5,12 +5,14 @@ namespace WebAPIAutores.DTOs
 	public class LlaveDTO
 	{
 		[Required]
-		public int Id{ get; set; }
+		public int Id { get; set; }
 
 		public string Llave { get; set; }
 
 		public bool Activa { get; set; }
 
 		public string TipoLlave { get; set; }
+
+		public List<RestriccionDominioDTO> RestriccionesDominio { get; set; }
 	}
 }
