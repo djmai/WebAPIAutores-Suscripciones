8c5a7aa22d5130ae3aa288b20d1349f2f984895d
Miguel Martínez <mai.djmai@gmail.com>
2023-11-07 12:35:52 -0600

    Agregando funcion de PeticionSuperaAlgunaDeLasRestricciones

--- Middlewares/LimitarPeticionesMiddlewareExtensions.cs
+++ Middlewares/LimitarPeticionesMiddlewareExtensions.cs
@@ -55,7 +55,10 @@ namespace WebAPIAutores.Middlewares
 
 			var llave = llaveStringValues[0];
 
-			var llaveDB = await context.LlavesAPI.FirstOrDefaultAsync(x => x.Llave == llave);
+			var llaveDB = await context.LlavesAPI
+				.Include(x => x.RestriccionesDominio)
+				.Include(x => x.RestriccionesIP)
+				.FirstOrDefaultAsync(x => x.Llave == llave);
 
 			if (llaveDB == null)
 			{
@@ -87,11 +90,50 @@ namespace WebAPIAutores.Middlewares
 				}
 			}
 
+			var superaRestricciones = PeticionSuperaAlgunaDeLasRestricciones(llaveDB, httpContext);
+			if (!superaRestricciones)
+			{
+				httpContext.Response.StatusCode = 403;
+				return;
+			}
+
 			var peticion = new Peticion() { LlaveId = llaveDB.Id, FechaPeticion = DateTime.UtcNow };
 			context.Add(peticion);
 			await context.SaveChangesAsync();
 
 			await siguiente(httpContext);
+		}
+
+		private bool PeticionSuperaAlgunaDeLasRestricciones(LlaveAPI llaveAPI, HttpContext httpContext)
+		{
+			var hayRestricciones = llaveAPI.RestriccionesDominio.Any() || llaveAPI.RestriccionesIP.Any();
+
+			if (!hayRestricciones)
+				return true;
+
+			var peticionSuperaLasRestriccionesDeDominio = PeticionSuperaLasRestriccionesDeDominio(llaveAPI.RestriccionesDominio, httpContext);
+
+			return peticionSuperaLasRestriccionesDeDominio;
+		}
+
+		private bool PeticionSuperaLasRestriccionesDeDominio(List<RestriccionDominio> restricciones, HttpContext httpContext)
+		{
+			if (restricciones == null || restricciones.Count == 0)
+				return false;
+
+			var referer = httpContext.Request.Headers["Referer"].ToString();
+
+			if (referer == string.Empty)
+				return false;
+
+			Uri myUri = new Uri(referer);
+
+			string host = myUri.Host;
+
+			var superaRestriccion = restricciones.Any(x => x.Dominio == host);
+
+			return superaRestriccion;
+
 		}
 	}
 }
